91ff14b43f95729088610b502bd736de81795986
Richard Herald <ric.herald@gmail.com>
2020-12-06 14:49:45 -0300

    feat: add lnpay client

---
+++ lib/by/lnpay/client.ex
@@ -0,0 +1,44 @@
+defmodule BY.Lnpay.Client do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://lnpay.co/v1/"
+  plug Tesla.Middleware.JSON
+
+  plug Tesla.Middleware.BasicAuth, username: @token
+
+  @token ""
+  @wallet_invoice_id ""
+  @wallet_admin_id ""
+
+  def generate_invoice(num_satoshis, memo) do
+    body = %{num_satoshis: num_satoshis, memo: memo}
+
+    "wallet/#{@wallet_invoice_id}/invoice"
+    |> post(body)
+    |> handle_generate_invoice()
+  end
+
+  defp handle_generate_invoice({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+  defp handle_generate_invoice({:ok, %Tesla.Env{status: 201, body: body}}), do: {:ok, body}
+  defp handle_generate_invoice({:ok, %Tesla.Env{status: 401, body: body}}), do: {:error, body}
+  defp handle_generate_invoice({:ok, %Tesla.Env{status: 404, body: body}}), do: {:error, body}
+
+  def get_invoice() do
+    "lntx/lntx_dPs4PNKiaXUHoHqZsroVcK9g"
+    |> get()
+    |> handle_get_invoice()
+  end
+
+  defp handle_get_invoice({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+  defp handle_get_invoice({:ok, %Tesla.Env{status: 401, body: body}}), do: {:error, body}
+  defp handle_get_invoice({:ok, %Tesla.Env{status: 404, body: body}}), do: {:error, body}
+
+  def get_withdraw_link(num_satoshis, memo) do
+    get("wallet/#{@wallet_admin_id}/lnurl/withdraw", query: [num_satoshis: num_satoshis, memo: memo])
+    |> handle_get_invoice()
+  end
+
+  defp handle_get_withdraw_link({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+  defp handle_get_withdraw_link({:ok, %Tesla.Env{status: 401, body: body}}), do: {:error, body}
+  defp handle_get_withdraw_link({:ok, %Tesla.Env{status: 404, body: body}}), do: {:error, body}
+end
--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule BY.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.4.0"}
     ]
   end
 
