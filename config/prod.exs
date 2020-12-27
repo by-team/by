634b529801370773a19de8baea80c90562f79014
João Thallis <joaothallis@icloud.com>
2020-12-27 18:38:43 -0300

    Configure to deploy in Gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :by, BYWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: System.get_env("HOST"), port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
+config :by, BY.Repo, ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
